b6f5fc9f13a4d66d3e95a7e19bec1bae29429496
Phil West <philowest@users.noreply.github.com>
2015-05-22 14:28:03 +0200

    Set test connection message color correctly.

--- src/main/java/jenkins/plugins/slack/SlackNotifier.java
+++ src/main/java/jenkins/plugins/slack/SlackNotifier.java
@@ -207,7 +207,7 @@ public class SlackNotifier extends Notifier {
             try {
                 SlackService testSlackService = getSlackService(teamDomain, authToken, room);
                 String message = "Slack/Jenkins plugin: you're all set on " + buildServerUrl;
-                boolean success = testSlackService.publish(message, "green");
+                boolean success = testSlackService.publish(message, "good");
                 return success ? FormValidation.ok("Success") : FormValidation.error("Failure");
             } catch (Exception e) {
                 return FormValidation.error("Client error : " + e.getMessage());
@@ -394,7 +394,7 @@ public class SlackNotifier extends Notifier {
                 try {
                     SlackService testSlackService = new StandardSlackService(teamDomain, authToken, room);
                     String message = "Slack/Jenkins plugin: you're all set.";
-                    boolean success = testSlackService.publish(message, "green");
+                    boolean success = testSlackService.publish(message, "good");
                     return success ? FormValidation.ok("Success") : FormValidation.error("Failure");
                 } catch (Exception e) {
                     return FormValidation.error("Client error : " + e.getMessage());
