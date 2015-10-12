665b83463d923e99ca51bcb0072f046fee86dc9f
Shovinus <baileyjoseph87@hotmail.com>
2015-10-12 10:42:01 +0100

    [FIX] Add prefix to stub

--- src/test/java/jenkins/plugins/slack/SlackNotifierStub.java
+++ src/test/java/jenkins/plugins/slack/SlackNotifierStub.java
@@ -6,10 +6,10 @@ public class SlackNotifierStub extends SlackNotifier {
                              String sendAs, boolean startNotification, boolean notifyAborted, boolean notifyFailure,
                              boolean notifyNotBuilt, boolean notifySuccess, boolean notifyUnstable, boolean notifyBackToNormal,
                              boolean notifyRepeatedFailure, boolean includeTestSummary, boolean showCommitList,
-                             boolean includeCustomMessage, String customMessage) {
+                             boolean includeCustomMessage, String customMessage, String jobPrefix) {
         super(teamDomain, authToken, room, buildServerUrl, sendAs, startNotification, notifyAborted, notifyFailure,
                 notifyNotBuilt, notifySuccess, notifyUnstable, notifyBackToNormal, notifyRepeatedFailure,
-                includeTestSummary, showCommitList, includeCustomMessage, customMessage);
+                includeTestSummary, showCommitList, includeCustomMessage, customMessage, jobPrefix);
     }
 
     public static class DescriptorImplStub extends SlackNotifier.DescriptorImpl {
