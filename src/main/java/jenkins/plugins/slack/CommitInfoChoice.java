5f10a91c39f41d4e60db19da5e684cd3b79c90ad
Andrey Mazo <ahippo@yandex.com>
2015-10-17 01:21:00 +0300

    CommitInfoChoice: default impl for showAuthor() and showTitle()
    This is a shorter and cleaner way.
    Thanks to @tterrag1098 for the suggestion!

--- src/main/java/jenkins/plugins/slack/CommitInfoChoice.java
+++ src/main/java/jenkins/plugins/slack/CommitInfoChoice.java
@@ -1,47 +1,26 @@
 package jenkins.plugins.slack;
 
 public enum CommitInfoChoice {
-    NONE("nothing about commits") {
-        @Override
-        public boolean showAuthor() {
-            return false;
-        }
-        @Override
-        public boolean showTitle() {
-            return false;
-        }
-    },
-
-    AUTHORS("commit list with authors only") {
-        @Override
-        public boolean showAuthor() {
-            return true;
-        }
-        @Override
-        public boolean showTitle() {
-            return false;
-        }
-    },
-
-    AUTHORS_AND_TITLES("commit list with authors and titles") {
-        @Override
-        public boolean showAuthor() {
-            return true;
-        }
-        @Override
-        public boolean showTitle() {
-            return true;
-        }
-    };
+    NONE("nothing about commits",                             false, false),
+    AUTHORS("commit list with authors only",                  true,  false),
+    AUTHORS_AND_TITLES("commit list with authors and titles", true,  true);
 
     private final String displayName;
+    private boolean showAuthor;
+    private boolean showTitle;
 
-    private CommitInfoChoice(String displayName) {
+    private CommitInfoChoice(String displayName, boolean showAuthor, boolean showTitle) {
         this.displayName = displayName;
+        this.showAuthor = showAuthor;
+        this.showTitle = showTitle;
     }
 
-    public abstract boolean showAuthor();
-    public abstract boolean showTitle();
+    public boolean showAuthor() {
+        return this.showAuthor;
+    }
+    public boolean showTitle() {
+        return this.showTitle;
+    }
     public boolean showAnything() {
         return showAuthor() || showTitle();
     }
