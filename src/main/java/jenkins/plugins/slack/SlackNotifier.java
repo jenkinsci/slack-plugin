4d82c72987adbeeeb41866c0934f482f9467f14e
Noah Castro <ncastro@contractor.ea.com>
2015-12-14 09:36:36 -0500

    Created an Empty Constructor so this can be used in a groovy script.
    Revision to remove unwanted whitespace and brackets.
    
    Returning brackets due to Jenkins check.

--- src/main/java/jenkins/plugins/slack/SlackNotifier.java
+++ src/main/java/jenkins/plugins/slack/SlackNotifier.java
@@ -124,6 +124,8 @@ public class SlackNotifier extends Notifier {
         return customMessage;
     }
 
+    public SlackNotifier(){}
+
     @DataBoundConstructor
     public SlackNotifier(final String teamDomain, final String authToken, final String room, final String buildServerUrl,
                          final String sendAs, final boolean startNotification, final boolean notifyAborted, final boolean notifyFailure,
