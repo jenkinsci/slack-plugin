16bad7fd2d4473ee96dd3a5e2eb663c59a8a3bcc
Kurt Madel <kmadel@mac.com>
2015-12-14 16:56:23 -0500

    added null check for Jenkins.getInstance(), introduced new set of true unit tests to allow testing for nul Jenkins.getInstance(), fixed some errors in help

--- src/main/java/jenkins/plugins/slack/workflow/SlackSendStep.java
+++ src/main/java/jenkins/plugins/slack/workflow/SlackSendStep.java
@@ -2,6 +2,7 @@ package jenkins.plugins.slack.workflow;
 
 import hudson.AbortException;
 import hudson.Extension;
+import hudson.Util;
 import hudson.model.TaskListener;
 import jenkins.model.Jenkins;
 import jenkins.plugins.slack.Messages;
@@ -23,22 +24,63 @@ import javax.inject.Inject;
  */
 public class SlackSendStep extends AbstractStepImpl {
 
-    public final String message;
+    private final @Nonnull String message;
+    private String color;
+    private String token;
+    private String channel;
+    private String teamDomain;
+    private boolean failOnError;
+
+
+    @Nonnull
+    public String getMessage() {
+        return message;
+    }
+
+    public String getColor() {
+        return color;
+    }
 
     @DataBoundSetter
-    public String color;
+    public void setColor(String color) {
+        this.color = Util.fixEmpty(color);
+    }
+
+    public String getToken() {
+        return token;
+    }
 
     @DataBoundSetter
-    public String token;
+    public void setToken(String token) {
+        this.token = Util.fixEmpty(token);
+    }
+
+    public String getChannel() {
+        return channel;
+    }
 
     @DataBoundSetter
-    public String channel;
+    public void setChannel(String channel) {
+        this.channel = Util.fixEmpty(channel);
+    }
+
+    public String getTeamDomain() {
+        return teamDomain;
+    }
 
     @DataBoundSetter
-    public String teamDomain;
+    public void setTeamDomain(String teamDomain) {
+        this.teamDomain = Util.fixEmpty(teamDomain);
+    }
+
+    public boolean isFailOnError() {
+        return failOnError;
+    }
 
     @DataBoundSetter
-    public boolean failOnError;
+    public void setFailOnError(boolean failOnError) {
+        this.failOnError = failOnError;
+    }
 
     @DataBoundConstructor
     public SlackSendStep(@Nonnull String message) {
@@ -68,16 +110,24 @@ public class SlackSendStep extends AbstractStepImpl {
         private static final long serialVersionUID = 1L;
 
         @Inject
-        private transient SlackSendStep step;
+        transient SlackSendStep step;
 
         @StepContextParameter
-        private transient TaskListener listener;
+        transient TaskListener listener;
 
         @Override
         protected Void run() throws Exception {
 
             //default to global config values if not set in step, but allow step to override all global settings
-            SlackNotifier.DescriptorImpl slackDesc = Jenkins.getInstance().getDescriptorByType(SlackNotifier.DescriptorImpl.class);
+            Jenkins jenkins;
+            //Jenkins.getInstance() may return null, no message sent in that case
+            try {
+                jenkins = Jenkins.getInstance();
+            } catch (NullPointerException ne) {
+                listener.error(Messages.NotificationFailedWithException(ne));
+                return null;
+            }
+            SlackNotifier.DescriptorImpl slackDesc = jenkins.getDescriptorByType(SlackNotifier.DescriptorImpl.class);
             String team = step.teamDomain != null ? step.teamDomain : slackDesc.getTeamDomain();
             String token = step.token != null ? step.token : slackDesc.getToken();
             String channel = step.channel != null ? step.channel : slackDesc.getRoom();
@@ -86,14 +136,19 @@ public class SlackSendStep extends AbstractStepImpl {
             //placing in console log to simplify testing of retrieving values from global config or from step field; also used for tests
             listener.getLogger().println(Messages.SlackSendStepConfig(step.teamDomain == null, step.token == null, step.channel == null, step.color == null));
 
-            SlackService testSlackService = new StandardSlackService(team, token, channel);
-            boolean publishSuccess = testSlackService.publish(step.message, color);
+            SlackService slackService = getSlackService(team, token, channel);
+            boolean publishSuccess = slackService.publish(step.message, color);
             if (!publishSuccess && step.failOnError) {
                 throw new AbortException(Messages.NotificationFailed());
             } else if (!publishSuccess) {
                 listener.error(Messages.NotificationFailed());
             }
             return null;
+        }
+
+        //streamline unit testing
+        SlackService getSlackService(String team, String token, String channel) {
+            return new StandardSlackService(team, token, channel);
         }
 
     }
