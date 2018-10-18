7a2b466a3ebdfb0009a3669fc95c207541623bc6
caguilar187 <caguilar187@gmail.com>
2018-10-18 17:18:58 -0400

    use powermockity to mock TokenMacro and test the slack extension

--- src/test/java/jenkins/plugins/slack/extension/SlackMessageExtensionTest.java
+++ src/test/java/jenkins/plugins/slack/extension/SlackMessageExtensionTest.java
@@ -3,7 +3,6 @@ package jenkins.plugins.slack.extension;
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.model.AbstractBuild;
-import hudson.model.Build;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.ItemGroup;
@@ -12,18 +11,28 @@ import jenkins.model.Jenkins;
 import jenkins.plugins.slack.ActiveNotifier;
 import jenkins.plugins.slack.SlackNotifier;
 import junit.framework.Assert;
+import org.jenkinsci.plugins.tokenmacro.MacroEvaluationException;
+import org.jenkinsci.plugins.tokenmacro.TokenMacro;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import java.io.IOException;
 import java.util.List;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.startsWith;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(TokenMacro.class)
 public class SlackMessageExtensionTest {
 
     private Jenkins jenkins;
@@ -43,10 +52,11 @@ public class SlackMessageExtensionTest {
     }
 
     @Test
-    public void testReplacement() throws IOException, InterruptedException {
+    public void testReplacement() throws IOException, InterruptedException, MacroEvaluationException {
         FreeStyleBuild build = Mockito.mock(FreeStyleBuild.class);
         FreeStyleProject project = Mockito.mock(FreeStyleProject.class);
         SlackNotifier notifier = Mockito.mock(SlackNotifier.class);
+        PowerMockito.mockStatic(TokenMacro.class);
 
         Mockito.when(build.getProject()).thenReturn(project);
         EnvVars vars = Mockito.mock(EnvVars.class);
@@ -60,16 +70,18 @@ public class SlackMessageExtensionTest {
             }
         });
 
-
         ItemGroup ig = Mockito.mock(ItemGroup.class);
         Mockito.when(ig.getFullDisplayName()).thenReturn("");
         Mockito.when(project.getParent()).thenReturn(ig);
         Mockito.when(project.getDisplayName()).thenReturn("project");
+        BDDMockito.when(TokenMacro.expandAll(any(FreeStyleBuild.class),any(TaskListener.class),startsWith("100%"))).thenReturn("100%");
 
         Mockito.when(notifier.getCustomMessage()).thenReturn("${INTERNAL_STRING}");
 
         ActiveNotifier.MessageBuilder messageBuilder = new ActiveNotifier.MessageBuilder(notifier, build);
         messageBuilder.appendCustomMessage();
+
+        PowerMockito.verifyStatic();
 
         String expectedResult = "project - #43 Started by changes from Bob \n100%";
 
