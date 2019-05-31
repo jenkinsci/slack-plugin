77ef960cb0d9f9300176b2ca6fe68889e86dc8ea
Eric Bartusch <ericbartusch8@gmail.com>
2019-05-31 15:11:46 -0500

    fixing compile error

--- src/test/java/jenkins/plugins/slack/SlackNotifierStub.java
+++ src/test/java/jenkins/plugins/slack/SlackNotifierStub.java
@@ -6,12 +6,12 @@ import jenkins.plugins.slack.matrix.MatrixTriggerMode;
 public class SlackNotifierStub extends SlackNotifier {
 
     public SlackNotifierStub(String baseUrl, String teamDomain, String authToken, boolean botUser, String room, String authTokenCredentialId,
-                             String sendAs, boolean startNotification, boolean notifyAborted, boolean notifyFailure,
+                             String sendAs, String iconEmoji, String username, boolean startNotification, boolean notifyAborted, boolean notifyFailure,
                              boolean notifyNotBuilt, boolean notifySuccess, boolean notifyUnstable, boolean notifyRegression, boolean notifyBackToNormal,
                              boolean notifyRepeatedFailure, boolean includeTestSummary, boolean includeFailedTests, MatrixTriggerMode matrixTriggerMode,
                              CommitInfoChoice commitInfoChoice, boolean includeCustomMessage, String customMessage, String customMessageSuccess,
                              String customMessageAborted, String customMessageNotBuilt, String customMessageUnstable, String customMessageFailure) {
-        super(baseUrl, teamDomain, authToken, botUser, room, authTokenCredentialId, sendAs, startNotification, notifyAborted, notifyFailure,
+        super(baseUrl, teamDomain, authToken, botUser, room, authTokenCredentialId, sendAs, iconEmoji, username, startNotification, notifyAborted, notifyFailure,
                 notifyNotBuilt, notifySuccess, notifyUnstable, notifyRegression, notifyBackToNormal, notifyRepeatedFailure,
                 includeTestSummary, includeFailedTests, matrixTriggerMode, commitInfoChoice, includeCustomMessage, customMessage,
                 customMessageSuccess, customMessageAborted, customMessageNotBuilt, customMessageUnstable, customMessageFailure);
