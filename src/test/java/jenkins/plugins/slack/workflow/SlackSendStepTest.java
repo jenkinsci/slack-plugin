2aca36462fb66b3d0389a8656b0e57243e108a0d
Stuart Rowe <stuartr@ea.com>
2019-07-18 11:20:57 -0700

    Fix checkstyle error - Unused import - org.powermock.core.classloader.annotations.PowerMockIgnore.

--- src/test/java/jenkins/plugins/slack/workflow/SlackSendStepTest.java
+++ src/test/java/jenkins/plugins/slack/workflow/SlackSendStepTest.java
@@ -22,7 +22,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
