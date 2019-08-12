5d56cc20b1e92ff9e99bf523f6fa7ab33dc682f7
Tim Jacomb <tim.jacomb@hmcts.net>
2019-08-12 08:44:15 +0100

    Fix WebhookEndpointTest

--- src/test/java/jenkins/plugins/slack/webhook/WebhookEndpointTest.java
+++ src/test/java/jenkins/plugins/slack/webhook/WebhookEndpointTest.java
@@ -26,6 +26,8 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -50,7 +52,7 @@ public class WebhookEndpointTest {
         data = new SlackPostData();
         data.setToken("GOOD_TOKEN");
         data.setTrigger_word("jenkins");
-        when(req.bindJSON(Mockito.eq(SlackPostData.class), Mockito.any(JSONObject.class))).thenReturn(data);
+        when(req.bindJSON(eq(SlackPostData.class), any())).thenReturn(data);
     }
 
     @Test
@@ -135,7 +137,7 @@ public class WebhookEndpointTest {
     }
 
     @Test
-    public void testRunNonExistantProject() throws Exception {
+    public void testRunNonExistentProject() throws Exception {
         setConfigSettings();
         data.setText("jenkins run project-1");
         JsonResponse response = (JsonResponse) endpoint.doIndex(req);
@@ -154,7 +156,7 @@ public class WebhookEndpointTest {
     }
 
     @Test
-    public void testGetProjectBuildLogWithNonExistantProject() throws Exception {
+    public void testGetProjectBuildLogWithNonExistentProject() throws Exception {
         setConfigSettings();
         data.setText("jenkins get project_1 #1 log");
         JsonResponse response = (JsonResponse) endpoint.doIndex(req);
