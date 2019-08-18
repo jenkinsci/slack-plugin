f997817a2c182e6aa1358ec87f63747c92b0c775
Tim Jacomb <tim.jacomb@hmcts.net>
2019-08-18 11:02:22 +0100

    Fix format

--- src/test/java/jenkins/plugins/slack/workflow/SlackSendStepIntegrationTest.java
+++ src/test/java/jenkins/plugins/slack/workflow/SlackSendStepIntegrationTest.java
@@ -11,8 +11,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
-public class
-SlackSendStepIntegrationTest {
+public class SlackSendStepIntegrationTest {
     @Rule
     public JenkinsRule jenkinsRule = new JenkinsRule();
 
