664f90457a37859cbc0d315a6bcadc02ab872d07
Jochen Schalanda <jochen@schalanda.name>
2021-05-05 13:38:44 +0200

    Add test for missing authentication token

--- src/test/java/jenkins/plugins/slack/user/EmailSlackUserIdResolverTest.java
+++ src/test/java/jenkins/plugins/slack/user/EmailSlackUserIdResolverTest.java
@@ -112,6 +112,17 @@ public class EmailSlackUserIdResolverTest {
     }
 
     @Test
+    public void testResolveUserIdWithoutAuthToken() throws Exception {
+        mailAddressResolver = mock(MailAddressResolver.class);
+        resolver = new EmailSlackUserIdResolver(AUTH_TOKEN, httpClient, Collections.singletonList(mailAddressResolver), user -> EMAIL_ADDRESS);
+        resolver.setAuthToken(null);
+        httpClient.setHttpResponse(getResponseOK());
+
+        String userId = resolver.resolveUserId(mock(User.class));
+        assertNull(userId);
+    }
+
+    @Test
     public void testResolveUserIdForUserWithoutResolver() throws Exception {
 
         resolver = new EmailSlackUserIdResolver(AUTH_TOKEN, httpClient, null, user -> {return EMAIL_ADDRESS;});
