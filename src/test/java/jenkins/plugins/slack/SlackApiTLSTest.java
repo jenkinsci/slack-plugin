955756034a9addb3fbbed7b741c564a36b112a4b
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-06-25 13:34:22 +0100

    Remove SSL test that fails on newer JDK (#770)

--- src/test/java/jenkins/plugins/slack/SlackApiTLSTest.java
+++ src/test/java/jenkins/plugins/slack/SlackApiTLSTest.java
@@ -1,21 +1,14 @@
 package jenkins.plugins.slack;
 
 import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLException;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.fail;
 
 public class SlackApiTLSTest {
 
@@ -29,26 +22,6 @@ public class SlackApiTLSTest {
             try (CloseableHttpResponse response = httpClient.execute(post)) {
                 StatusLine statusLine = response.getStatusLine();
                 assertThat(statusLine.getStatusCode(), is(200));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void connectToAPIUsingTLS11() throws NoSuchAlgorithmException {
-        final HttpClientBuilder clientBuilder = HttpClients
-            .custom()
-            .setSSLSocketFactory(new SSLConnectionSocketFactory(SSLContext.getDefault(),
-                new String[] { "TLSv1.1" }, null,
-                SSLConnectionSocketFactory.getDefaultHostnameVerifier()));
-        try (CloseableHttpClient httpClient = clientBuilder.build()) {
-            HttpPost post = new HttpPost(SLACK_API_TEST);
-            post.setHeader("Content-Type", "application/json; charset=utf-8");
-            try (CloseableHttpResponse ignored = httpClient.execute(post)) {
-                fail("Slack no longer accepts anything below TLSv1.2");
-            } catch (SSLException ex) {
-                assertThat(ex.getMessage(), is("Received fatal alert: protocol_version"));
             }
         } catch (IOException e) {
             e.printStackTrace();
